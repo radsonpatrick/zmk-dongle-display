1f8d9d69cb8e399a765382d361d41c9c8dec98ba
Radson Almada <43654273+radsonpatrick@users.noreply.github.com>
2024-11-11 12:56:18 -0300

    alteration

--- boards/shields/dongle_display/widgets/output_status.c
+++ boards/shields/dongle_display/widgets/output_status.c
@@ -177,9 +177,10 @@ int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_ob
     lv_obj_align_to(usb_hid_status, usb, LV_ALIGN_BOTTOM_LEFT, 2, -7);
 
     lv_obj_t *bt = lv_img_create(widget->obj);
+    lv_img_set_angle(bt, 900);
     lv_obj_align_to(bt, usb, LV_ALIGN_OUT_RIGHT_TOP, 6, 0);
     lv_img_set_src(bt, &sym_bt);
-    lv_img_set_angle(bt, 900);
+    
 
     lv_obj_t *bt_number = lv_img_create(widget->obj);
     lv_obj_align_to(bt_number, bt, LV_ALIGN_OUT_RIGHT_TOP, 2, 7);
