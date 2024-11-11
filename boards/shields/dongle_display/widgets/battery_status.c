0c930496887f447282f48d15267d566ae6fa527c
Radson Almada <43654273+radsonpatrick@users.noreply.github.com>
2024-11-11 12:13:39 -0300

    fix widget_output only

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -143,7 +143,6 @@ int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_statu
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 5, 8, LV_IMG_CF_TRUE_COLOR);
         lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, 0, i * 10);
         lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, i * 10);
-
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
     }
