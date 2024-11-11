53e40931bec1d4769b09ff07dfc22633f6eff646
Radson Almada <43654273+radsonpatrick@users.noreply.github.com>
2024-11-11 18:50:20 -0300

    test rotate bluetooth array byte

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -37,7 +37,6 @@ lv_obj_t *zmk_display_status_screen() {
     lv_style_set_text_letter_space(&global_style, 1);
     lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
-    lv_display_set_resolution(screen,32,128);
     zmk_widget_output_status_init(&output_status_widget, screen);
     lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
     
