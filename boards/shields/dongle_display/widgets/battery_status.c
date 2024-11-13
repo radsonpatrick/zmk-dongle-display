e991b95c872cda91e4e8d29d8e7d39f3a60fa266
Radson Almada <43654273+radsonpatrick@users.noreply.github.com>
2024-11-13 15:36:59 -0300

    test

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -142,7 +142,7 @@ int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_statu
 
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 20, 10, LV_IMG_CF_TRUE_COLOR);
         lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, i * 20,0);
-        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, i * 10);
+        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -20, i * 20);
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
     }
