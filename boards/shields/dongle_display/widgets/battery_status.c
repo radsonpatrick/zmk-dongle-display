87a4b4f4c2af82813cc7536fe7c04b7b08147b3a
Radson Almada <43654273+radsonpatrick@users.noreply.github.com>
2024-11-13 22:24:53 -0300

    k

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -73,7 +73,7 @@ static void set_battery_symbol(lv_obj_t *widget, struct battery_state state) {
     lv_obj_t *label = lv_obj_get_child(widget, state.source * 2 + 1);
 
     draw_battery(symbol, state.level, state.usb_present);
-    //lv_label_set_text_fmt(label, "%4u%%", state.level);
+    lv_label_set_text_fmt(label, "%4u%%", state.level);
     
     if (state.level > 0 || state.usb_present) {
         lv_obj_clear_flag(symbol, LV_OBJ_FLAG_HIDDEN);
@@ -143,10 +143,10 @@ int zmk_widget_dongle_battery_status_init(struct zmk_widget_dongle_battery_statu
         lv_canvas_set_buffer(image_canvas, battery_image_buffer[i], 20, 10, LV_IMG_CF_TRUE_COLOR);
 
         lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, 0, i * 20);
-        //lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, i * 20);
+        lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, i * 20);
 
         lv_obj_add_flag(image_canvas, LV_OBJ_FLAG_HIDDEN);
-        //lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(battery_label, LV_OBJ_FLAG_HIDDEN);
     }
 
     sys_slist_append(&widgets, &widget->node);
